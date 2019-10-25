df8ab5a7174b58e8048dc1014cc6f42cd555173a
Kanta Demizu <demizu.1203@gmail.com>
2019-10-25 23:32:03 +0900

    [ADD] Suggest by FlexMessage

--- main.go
+++ main.go
@@ -89,9 +89,8 @@ func main() {
 				case *linebot.TextMessage:
 					_, err = bot.ReplyMessage(
 						event.ReplyToken,
-						linebot.NewTextMessage(
-							message.Text),
-						).Do()
+						linebot.NewTextMessage(message.Text),
+					).Do()
 					if err != nil {
 						log.Fatal(err)
 					}
@@ -99,10 +98,132 @@ func main() {
 				case *linebot.LocationMessage:
 					lat, lon := message.Latitude, message.Longitude
 					restaurants := retrieveRestaurants(lat, lon)
+
 					if len(restaurants.Rest) > 0 {
+						var flexColumns []*linebot.BubbleContainer
+						for i := range restaurants.Rest{
+							var flexContent = &linebot.BubbleContainer{
+								Type:   linebot.FlexContainerTypeBubble,
+								Body:	&linebot.BoxComponent{
+									Type: 		linebot.FlexComponentTypeBox,
+									Layout:		linebot.FlexBoxLayoutTypeVertical,
+									Contents:	[]linebot.FlexComponent{
+										&linebot.TextComponent{
+											Type:     linebot.FlexComponentTypeText,
+											Text:     restaurants.Rest[i].Name,
+											Weight:   linebot.FlexTextWeightTypeBold,
+											Size:     linebot.FlexTextSizeTypeXl,
+										},
+										&linebot.BoxComponent{
+											Type:     linebot.FlexComponentTypeBox,
+											Layout:   linebot.FlexBoxLayoutTypeVertical,
+											Contents: []linebot.FlexComponent{
+												&linebot.BoxComponent{
+													Type:     linebot.FlexComponentTypeBox,
+													Layout:   linebot.FlexBoxLayoutTypeBaseline,
+													Contents: []linebot.FlexComponent{
+														&linebot.TextComponent{
+															Type:	linebot.FlexComponentTypeText,
+															Text:	"Access",
+															Size:	linebot.FlexTextSizeTypeSm,
+															Flex:	linebot.IntPtr(1),
+															Color:	"#aaaaaa",
+														},
+														&linebot.TextComponent{
+															Type:	linebot.FlexComponentTypeText,
+															Text:	fmt.Sprintf("%s %s %s 分", restaurants.Rest[i].Access.Line, restaurants.Rest[i].Access.Station, restaurants.Rest[i].Access.Walk),
+															Wrap:	true,
+															Size:	linebot.FlexTextSizeTypeSm,
+															Flex:	linebot.IntPtr(5),
+															Color:	"#666666",
+														},
+													},
+													Spacing:  linebot.FlexComponentSpacingTypeSm,
+												},
+												&linebot.BoxComponent{
+													Type:     linebot.FlexComponentTypeBox,
+													Layout:   linebot.FlexBoxLayoutTypeBaseline,
+													Contents: []linebot.FlexComponent{
+														&linebot.TextComponent{
+															Type:	linebot.FlexComponentTypeText,
+															Text:	"Place",
+															Size:	linebot.FlexTextSizeTypeSm,
+															Flex:	linebot.IntPtr(1),
+															Color:	"#aaaaaa",
+														},
+														&linebot.TextComponent{
+															Type:	linebot.FlexComponentTypeText,
+															Text:	restaurants.Rest[i].Address,
+															Wrap:	true,
+															Size:	linebot.FlexTextSizeTypeSm,
+															Flex:	linebot.IntPtr(5),
+															Color:	"#666666",
+														},
+													},
+													Spacing:  linebot.FlexComponentSpacingTypeSm,
+												},
+												&linebot.BoxComponent{
+													Type:     linebot.FlexComponentTypeBox,
+													Layout:   linebot.FlexBoxLayoutTypeBaseline,
+													Contents: []linebot.FlexComponent{
+														&linebot.TextComponent{
+															Type:	linebot.FlexComponentTypeText,
+															Text:	"Tel",
+															Size:	linebot.FlexTextSizeTypeSm,
+															Flex:	linebot.IntPtr(1),
+															Color:	"#aaaaaa",
+														},
+														&linebot.TextComponent{
+															Type:	linebot.FlexComponentTypeText,
+															Text:	restaurants.Rest[i].Tel,
+															Wrap:	true,
+															Size:	linebot.FlexTextSizeTypeSm,
+															Flex:	linebot.IntPtr(5),
+															Color:	"#666666",
+														},
+													},
+													Spacing:  linebot.FlexComponentSpacingTypeSm,
+												},
+											},
+											Margin:   linebot.FlexComponentMarginTypeLg,
+											Spacing:  linebot.FlexComponentSpacingTypeSm,
+										},
+									},
+								},
+								Footer:	&linebot.BoxComponent{
+									Type:     linebot.FlexComponentTypeBox,
+									Layout:   linebot.FlexBoxLayoutTypeVertical,
+									Contents: []linebot.FlexComponent{
+										&linebot.ButtonComponent{
+											Type:    linebot.FlexComponentTypeButton,
+											Action:  linebot.NewURIAction("WEBSITE", restaurants.Rest[i].URL),
+											Height:  linebot.FlexButtonHeightTypeSm,
+											Style:   linebot.FlexButtonStyleTypeLink,
+										},
+										&linebot.SpacerComponent{
+											Type: linebot.FlexComponentTypeSpacer,
+											Size: linebot.FlexSpacerSizeTypeSm,
+										},
+									},
+									Spacing:  linebot.FlexComponentSpacingTypeSm,
+									Flex:linebot.IntPtr(0),
+								},
+							}
+
+							flexColumns = append(flexColumns, flexContent)
+							if i == 5 {
+								break
+							}
+
+						}
+						carousel := &linebot.CarouselContainer{
+							Type:     linebot.FlexContainerTypeCarousel,
+							Contents: flexColumns,
+						}
 						_, err = bot.ReplyMessage(
 							event.ReplyToken,
-							linebot.NewTextMessage(restaurants.Rest[0].Name),
+							linebot.NewTextMessage("はいいらっしゃいラーメンﾄﾞｰｰｰｰｰﾝ！！！！！！！！！食え食え食え食え食え食え食え食え食った？食った？食った？食った？食った？くった？食った？食った？食った？食った？食った？出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ出てけ"),
+							linebot.NewFlexMessage("alt", carousel),
 						).Do()
 						if err != nil {
 							log.Fatal(err)
